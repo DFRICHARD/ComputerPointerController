48a76ff59475632573b2c5131a92c16d87070edc
DFRICHARD <54338939+DFRICHARD@users.noreply.github.com>
2020-08-02 22:29:23 +0100

    app with default parameters

--- src/app.py
+++ src/app.py
@@ -25,8 +25,8 @@ def build_argparser():
         'hpe_path': r'C:\Users\DZERICHARD\Desktop\Udacity Projects\ComputerPointerController\intel\head-pose-estimation-adas-0001\FP32\head-pose-estimation-adas-0001.xml',
         'fld_path': r'C:\Users\DZERICHARD\Desktop\Udacity Projects\ComputerPointerController\intel\landmarks-regression-retail-0009\FP32\landmarks-regression-retail-0009.xml',
         'ge_path': r'C:\Users\DZERICHARD\Desktop\Udacity Projects\ComputerPointerController\intel\gaze-estimation-adas-0002\FP32\gaze-estimation-adas-0002.xml',
-        'i':r'C:\Users\DZERICHARD\Desktop\Udacity Projects\ComputerPointerController\bin\demo.mp4',
-        # 'e': "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so"
+        'i':'demo.mp4'
+        # 'ext': "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so"
     }
 
     parser.add_argument("-fd", "--facedetection", required=False, default=default_paths['fd_path'], type=str,
@@ -42,18 +42,19 @@ def build_argparser():
                         help="Path to a gaze estimation model .xml file.")
 
     parser.add_argument("-i", "--input", required=False, type=str, default = default_paths['i'],
-                        help="Path to input file (image or video file or type cam)")
+                        help="Path to input file (image or video file or type 'cam')")
 
-    parser.add_argument("-ex", "--extension", required=False, type=str, default = None
+    parser.add_argument("-ex", "--extension", required=False, type=str, default = None,
                         help="MKLDNN (CPU)-targeted custom layers."
                              "Absolute path to a shared library with the"
                              "kernels impl.")
 
-    parser.add_argument("-dis", "--display", required=False, default=True, type=str,
+    parser.add_argument("-viz", "--display_flags", required=False, default = True, type=str,
                         help="Flag to display the outputs of the intermediate models")
 
     parser.add_argument("-d", "--device", type=str, default="CPU",
-                        help="Specify the target device to infer on: (CPU by default)")
+                        help="Specify the target device to infer on: CPU, GPU, FPGA or MYRIAD"
+                             "(CPU by default)")
 
     parser.add_argument("-pt_fd", "--fd_threshold", type=float, default=0.6,
                         help="Probability threshold for detections filtering"
@@ -73,7 +74,8 @@ def main():
             log.error("Unable to find specified input file")
             exit(1)
         Feed = InputFeeder("image", args.input)
-
+    elif args.input.endswith('.mp4'):
+        Feed = InputFeeder("video", args.input)
     else:
         if not os.path.isfile(args.input):
             log.error("Unable to find specified video file")
@@ -96,7 +98,10 @@ def main():
     gaze_est = GazeEstimation(args.gazeestimation, args.device, args.extension)
     gaze_est.load_model()
 
+    args.display_flags
+
     counter = 0
+
     for n, frame in Feed.next_batch():
         if frame is None:
             log.error('The input file is corrupted!!')
@@ -104,18 +109,21 @@ def main():
         if not n:
             break
 
+        image_copy = np.copy(frame)
         counter += 1
+
         key_pressed =cv2.waitKey(60)
-        face_crop, face_coords = face_det.predict(frame)
+        out_image, face_crop, face_coords = face_det.predict(image_copy, args.display_flags)
+
         if len(face_coords) == 0:
             log.error("No face detected")
             continue
 
-        hp_angles = head_pose.predict(face_crop)
-        l_eye, r_eye, eye_coords = face_landmark.predict(face_crop)
-        mouse_coords, gaze_vector = gaze_est.predict(frame, l_eye, r_eye, hp_angles)
+        out_image, hp_angles = head_pose.predict(out_image, face_crop, face_coords, args.display_flags)
+        out_image, l_eye, r_eye, eye_coords = face_landmark.predict(out_image, face_crop, args.display_flags)
+        mouse_coords, gaze_vector = gaze_est.predict(out_image, l_eye, r_eye, hp_angles, args.display_flags)
 
-        cv2.imshow('frame', cv2.resize(frame, (600, 400)))
+        cv2.imshow('frame', cv2.resize(out_image, (600, 400)))
         mc.move(mouse_coords[0], mouse_coords[1])
 
         if key_pressed == 27:
@@ -125,4 +133,5 @@ def main():
     Feed.close()
 
 
-if __name__ == '__main__
+if __name__ == '__main__':
+    main()
